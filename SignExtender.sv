a545ab3c0497ceafdb8241c5ed44df9f66f3c074
lziyang <zl4223@ic.ac.uk>
2024-11-23 19:13:56 +0000

    Update SignExtender.sv

--- SignExtender.sv
+++ SignExtender.sv
@@ -10,10 +10,10 @@ module SignExtender #(
 
 always_comb begin
     case (ImmSrc)
-        2'b00: ImmExt = {{20{Instr[31]}},Instr[WIDTH-1:20]}; // I-type
-        2'b01: ImmExt = {{20{Instr[31]}},Instr[WIDTH-1:25],Instr[11:7]};  // S-type
+        2'b00: ImmExt = {{20{Instr[31]}},Instr[31:20]}; // I-type
+        2'b01: ImmExt = {{20{Instr[31]}},Instr[31:25],Instr[11:7]};  // S-type
         2'b10: ImmExt = {{19{Instr[31]}},Instr[31],Instr[7],Instr[30:25],Instr[11:8],1'b0};  // B-type
-        2'b11: ImmExt = {{11{Instr[31]}}, Instr[30], Instr[19:12],Instr[20],Instr[30:21],1'b0};  // J-type
+        2'b11: ImmExt = {{11{Instr[31]}}, Instr[31], Instr[19:12],Instr[20],Instr[30:21],1'b0};  // J-type
         default: ImmExt = 32'b0
     endcase
 end
