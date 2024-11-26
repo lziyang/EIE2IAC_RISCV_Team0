e58d11683f4fcaf2f8c44536c14e369b3800ff0e
siyuanyue724 <140235546+siyuanyue724@users.noreply.github.com>
2024-11-26 06:19:10 +0000

    Update CUSE.sv

--- CUSE.sv
+++ CUSE.sv
@@ -11,13 +11,16 @@ module CUSE (
     output logic [1:0]  ResultSrc,
     output logic [3:0]  ALUControl,
     output logic [1:0]  ImmSrc,
-    output logic [31:0] ImmExt
+    output logic [31:0] ImmExt,
+    output logic [2:0]  DataControl  // New output for data handler
 );
-    // Instra extraction
+
+    // Instr extraction
     logic [6:0] op      = Instr[6:0];
     logic [2:0] funct3  = Instr[14:12];
     logic       funct7  = Instr[30];
 
+    // Instantiate ControlUnit
     ControlUnit control_unit (
         .op           (op),
         .funct3       (funct3),
@@ -32,13 +35,14 @@ module CUSE (
         .PCSrc        (PCSrc),
         .ResultSrc    (ResultSrc),
         .ALUControl   (ALUControl),
-        .ImmSrc       (ImmSrc)
+        .ImmSrc       (ImmSrc),
+        .DataControl  (DataControl)  // Connect DataControl signal
     );
 
+    // Instantiate SignExtender
     SignExtender sign_extender (
         .Instr   (Instr),
         .ImmSrc  (ImmSrc),
         .ImmExt  (ImmExt)
     );
 endmodule
-
