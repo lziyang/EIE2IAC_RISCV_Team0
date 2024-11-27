a2a38a9125ec231bc68b2468598812d42cddab11
lziyang <zl4223@ic.ac.uk>
2024-11-27 22:01:31 +0000

    Added default cases and updated memory mapping

--- DataMemory.sv
+++ DataMemory.sv
@@ -1,28 +1,23 @@
-module datamemory#(
-    parameter WIDTH = 32
-
-)(
-    input logic                     clk,
-    input logic                     MemWrite,
-    input logic                     ResultSrc,
-    input logic                     LoadSign,  // differentiates between zero extension (0) and sign extension (1)
-    input logic [1:0]               size,   // determines if it is byte/word/half
-    input logic [WIDTH-1:0]         WriteData, //data to be written to memory for store operations
-    input logic [WIDTH-1:0]         ALUResult, //memory address for read/write operators
-    output logic [WIDTH-1:0]        Result
+module DataMemory(
+    input logic                clk,
+    input logic                MemWrite,
+    input logic                LoadSign,  // differentiates between zero extension (0) and sign extension (1)
+    input logic [1:0]          SizeSrc,   // determines if it is byte/word/half
+    input logic [31:0]         WriteData, //data to be written to memory for store operations
+    input logic [16:0]         ALUResult, //memory address for read/write operators
+    output logic [31:0]        ReadData
 );
 
-logic [WIDTH-1:0] ReadData;                 // 32 bits = 4 bytes
-logic [7:0] memory [2**WIDTH-1:0];          // 8 bits = 1 byte
-logic [7:0]       address;
+logic [7:0] memory [2**17-1:0];          // 8 bits = 1 byte
+logic [16:0]       address;
 
 //asynchronous cycle
 //read operation as it automatic (clk can be ignored)
 
-assign address = ALUResult[7:0];   //from 0 to 27 for byte addressing and to be 28 bits
+assign address = ALUResult; 
 
 always_comb begin
-    case(size)
+    case(SizeSrc)
         2'b00: ReadData = {memory[address+3], memory[address+2], memory[address+1], memory[address]};    //lw and incrementing address
         2'b01: begin                                                          //lh and lhu
             case(LoadSign)                                                      
@@ -36,6 +31,7 @@ always_comb begin
                 1'b1: ReadData = {{24{memory[address][7]}}, memory[address]};
             endcase
         end
+        default: ReadData = 32'b0;
     endcase
 end
  
@@ -43,7 +39,7 @@ end
 //write operation as it is posedge clk triggered
 always_ff @(posedge clk) begin
     if(MemWrite) begin
-        case(size)
+        case(SizeSrc)
             2'b00: begin                            //sw
                 memory[address] <= WriteData[7:0];
                 memory[address+1] <= WriteData[15:8];
@@ -56,7 +52,8 @@ always_ff @(posedge clk) begin
             end         
             2'b10: begin              //sb
                 memory[address] <= WriteData[7:0];      
-            end                
+            end
+            default: ; // do nothing               
         endcase
     end
 end
