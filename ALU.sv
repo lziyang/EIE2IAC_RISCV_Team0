066b5ebbdc4f65909f63e8446ebe1a3aa81c9402
lziyang <zl4223@ic.ac.uk>
2024-11-27 20:18:45 +0000

    Upgraded ALU to include all RiscV32I instructions

--- ALU.sv
+++ ALU.sv
@@ -1,30 +1,38 @@
-module ALU #(
-    parameter DATAWIDTH = 32
-)(
-    input logic [DATAWIDTH-1:0] ALUop1,
-    input logic [DATAWIDTH-1:0] ALUop2,
-    input logic [3:0] ALUctrl,
-    output logic [DATAWIDTH-1:0] SUM,
-    output logic Zero
+module ALU(
+    input logic [31:0] SrcA,
+    input logic [31:0] SrcB,
+    input logic [3:0] ALUControl,
+    output logic [31:0] SUM,
+    output logic Zero,
+    output logic signedLess,
+    output logic unsignedLess
 );
 
     always_comb begin
-        case (ALUctrl)
-            4'b0000: SUM = ALUop1 + ALUop2;         
-            4'b0001: SUM = ALUop1 - ALUop2;             
-            4'b0010: SUM = ALUop1 & ALUop2;         
-            4'b0011: SUM = ALUop1 | ALUop2;
-            4'b0100: SUM = ALUop1 ^ ALUop2;
-            4'b0101: SUM = (ALUop1 < ALUop2) ? 1 : 0;
-            4'b0110: SUM = ($signed(ALUop1) < $signed(ALUop2)) ? 1 : 0;
-            4'b0111: SUM = ALUop2;
-            4'b1000: SUM = ALUop1 << ALUop2;
-            4'b1001: SUM = ALUop1 >> ALUop2;
-            4'b1010: SUM = ALUop1 >>> ALUop2;    
+
+        SUM = 0;
+        Zero = 0;
+
+        case (ALUControl)
+            4'b0000: SUM = SrcA + SrcB;         
+            4'b0001: begin
+                SUM = SrcA - SrcB;
+                Zero = (SUM == 0);
+            end
+            4'b0010: SUM = SrcA & SrcB;         
+            4'b0011: SUM = SrcA | SrcB;
+            4'b0100: SUM = SrcA ^ SrcB;
+            4'b0101: SUM = (SrcA < SrcB) ? 1 : 0;
+            4'b0110: SUM = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
+            4'b0111: SUM = SrcB;
+            4'b1000: SUM = SrcA << SrcB;
+            4'b1001: SUM = SrcA >> SrcB;
+            4'b1010: SUM = SrcA >>> SrcB;   
             default: SUM = 0;                     
         endcase
     end
 
-    assign Zero = (SUM == 0);
+    assign signedLess = ($signed(SrcA) < $signed(SrcB));
+    assign unsignedLess = (SrcA < SrcB);
 
 endmodule
