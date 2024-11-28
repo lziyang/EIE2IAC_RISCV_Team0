743032100b5e0d2859e8215109a40e406c4de1ed
lziyang <zl4223@ic.ac.uk>
2024-11-28 09:53:40 +0000

    Create new mux to handle jumps and other instructions accommodating DataMemory

---
+++ ALUMux2.sv
@@ -0,0 +1,18 @@
+module ALUMux2 (
+    input logic [1:0] ResultSrc,
+    input logic [31:0] ALUResult,
+    input logic [31:0] ReadData,
+    input logic [31:0] PCPlus4,
+    output logic [31:0] Result
+);
+
+always_comb begin
+    case (ResultSrc)
+        2'b00: Result = ALUResult;   
+        2'b01: Result = ReadData;     
+        2'b10: Result = PCPlus4; 
+        default: Result = 0;
+    endcase
+end
+
+endmodule
