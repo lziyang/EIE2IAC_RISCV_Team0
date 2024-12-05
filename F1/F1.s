799417560b26401ff94889400bb6c80ee8a3d193
lziyang <zl4223@ic.ac.uk>
2024-12-05 06:32:11 +0000

    Register usage shuffled around to make a0 the led state output

--- F1/F1.s
+++ F1/F1.s
@@ -1,10 +1,10 @@
 # reg list：
-# t0: current light status（1, 11, 111, ..., 0x1111_1111）
-# a0: final light status, 0xFF = 0b1111_1111
+# a0: current light status（1, 11, 111, ..., 0x1111_1111）
+# t0: final light status, 0xFF = 0b1111_1111
 # a1: random delay counter for the random delay after all lights are on
 # a2: time scale factor to extend cpu timer to real-life time
 # a3: rst
-# a4: trigger
+# t1: trigger
 # t3: random value seed (and random value itself).
 # t4: delay counter for 1 sec delay
 # t5: seed counter to generate seed
@@ -15,14 +15,14 @@
 #--------------------------------------------
 
 init:
-    addi t0, x0, 0          # init light status t0 = 0
-    addi a0, x0, 0xFF       # final light status
+    addi a0, x0, 0          # init light status a0 = 0
+    addi t0, x0, 0xFF       # final light status
     addi t3, x0, 0          # random value
     addi t5, x0, 0          # seed counter
 
 
     # 触发信号寄存器初始化
-    addi a4, x0, 0          # trigger
+    addi t1, x0, 0          # trigger
     
     # 注意，a2 (SCALE) and a3 (rst) 至少部分是由外部因素决定的，特别是rst可能会由某个外部方式来进行输入？暂时留白
     addi a2, x0, 16            # a2 (scale) will be set based on time_scale. For not set it as 16 to simplify testing
@@ -38,8 +38,8 @@ seed_generation:
     andi t5, t5, 0xFF       # remain t5 under 0xFF, this essentially serves as a pseudo loop
 
     # check for trigger
-    beq a4, x0, seed_generation  # continue looop if trigger is 0
-    addi a4, x0, 0          # set trigger to 0 if trigger is not 0 (wait for next trigger input) Notice glitch here! (if we pressed the trigger during the loop or smthing it will cause the trigger to remain as high. We can refine this later but remain it as this current version to enable easy testing)
+    beq t1, x0, seed_generation  # continue looop if trigger is 0
+    addi t1, x0, 0          # set trigger to 0 if trigger is not 0 (wait for next trigger input) Notice glitch here! (if we pressed the trigger during the loop or smthing it will cause the trigger to remain as high. We can refine this later but remain it as this current version to enable easy testing)
     addi t3, t5, 0          # save seed to t3
     jal x0, main            # go to main loop to turn on lights
 
@@ -47,21 +47,21 @@ seed_generation:
 # main loop to turn on lights
 #--------------------------------------------
 main:
-    # t0 is the light status
+    # a0 is the light status
     jal x1, delay           # go to delay function for 1 sec delay
-    slli t0, t0, 1          # t0 = t0 << 1
-    addi t0, t0, 1          # t0 = t0 + 1. With the line above to form new light status, i.e. 111 -> 1111 -> 11111 etc
+    slli a0, a0, 1          # a0 = a0 << 1
+    addi a0, a0, 1          # a0 = a0 + 1. With the line above to form new light status, i.e. 111 -> 1111 -> 11111 etc
     # check for rst (a3)
     bne a3, x0, init        # if rst is not 0, go to init
-    bne t0, a0, main        # if t0 (light status) is not yet a0 (0xFF, or 0x1111_1111), continue the main loop
-    beq t0, a0, all_lights_on  # once the condition above is satisfied, i.e. all lights are on, go to the all_lights_on function to turn off lights
+    bne a0, t0, main        # if a0 (light status) is not yet t0 (0xFF, or 0x1111_1111), continue the main loop
+    beq a0, t0, all_lights_on  # once the condition above is satisfied, i.e. all lights are on, go to the all_lights_on function to turn off lights
 
 #--------------------------------------------
 # to turn of the lights after random delay
 #--------------------------------------------
 all_lights_on:
     jal x1, random_delay    # use the random delay func
-    addi t0, x0, 0          # turn off all lights, i.e. t0 = 0x00
+    addi a0, x0, 0          # turn off all lights, i.e. a0 = 0x00
 
     jal x0, init            # go back to init
 
