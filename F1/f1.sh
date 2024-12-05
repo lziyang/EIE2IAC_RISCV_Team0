db2f49d694748c233937ac3de40b11d30c8d1aa3
lziyang <zl4223@ic.ac.uk>
2024-12-05 06:33:33 +0000

    Minor change of module name

--- F1/f1.sh
+++ F1/f1.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-
 ~/Documents/iac/lab0-devtools/tools/attach_usb.sh
 
 rm -rf obj_dir
-rm -f CPUTOP.vcd
+rm -f top.vcd
 
-verilator -Wall --cc --trace CPUTOP.sv --exe f1_tb.cpp
+verilator -Wall --cc --trace top.sv --exe f1_tb.cpp
 
-make -j -C obj_dir/ -f VCPUTOP.mk VCPUTOP
+make -j -C obj_dir/ -f Vtop.mk Vtop
 
-obj_dir/VCPUTOP
+obj_dir/Vtop
