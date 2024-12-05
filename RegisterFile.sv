dcd14541520f60ee6cebcec4c1856dec98e91413
lziyang <zl4223@ic.ac.uk>
2024-12-05 06:26:13 +0000

    Created a trigger write required for F1 program
    This implementation is quite specific for the written F1 program, not meant for other programs, will be edited

--- RegisterFile.sv
+++ RegisterFile.sv
@@ -5,16 +5,21 @@ module RegisterFile (
     input logic [4:0] A3,
     input logic [31:0] WD3,
     input logic WE3,
+    input logic trigger,
     output logic [31:0] a0,
     output logic [31:0] RD1,
     output logic [31:0] RD2
 );
 
     logic [31:0] registerarr [0:31];
-
     assign RD1 = registerarr[A1];
     assign RD2 = registerarr[A2];
     assign a0 = registerarr[10];
+
+    always_ff @(posedge clk) begin
+            assign registerarr[6] = trigger;
+
+    end
 
     always_ff @(posedge clk) begin
         if (WE3 && A3 != 0) begin //added x0 check so never change x0
